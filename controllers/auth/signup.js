946bb7f8fd287b2f0a7e5dd85a4b8f8624fbfca2
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-10-06 22:44:44 +0300

    signup response updated

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const { User } = require("../../models/user");
 const { RequestError } = require("../../helpers");
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.findOne({ email });
 
@@ -11,10 +11,17 @@ const register = async (req, res) => {
     throw RequestError(409, `User with ${email} already exist`);
   }
   const hashPsw = await bcrypt.hash(password, 10);
-  const result = await User.create({ name, email, password: hashPsw });
-  res.status(201).json({
-    email: result.email,
+  await User.create({
+    name,
+    email,
+    password: hashPsw,
   });
+  res.status(201);
+  next();
+  // const result = await User.create({ name, email, password: hashPsw });
+  // res.status(201).json({
+  //   email: result.email,
+  // });
 };
 
 module.exports = register;
