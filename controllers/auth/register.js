d85e1a2a09424646680a494d90973a4f23942715
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-10-17 12:23:41 +0300

    code cleaning

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,26 +1,3 @@
-// const bcrypt = require("bcryptjs");
-
-// const { User } = require("../../models/user");
-// const { Conflict } = require("http-errors");
-
-// const signup = async (req, res, next) => {
-//   const { email, password, name } = req.body;
-//   const user = await User.findOne({ email });
-//   if (user) {
-//     throw new Conflict(`User with ${email} already exist`);
-//   }
-//   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-//   await User.create({
-//     name,
-//     email,
-//     password: hashPassword,
-//   });
-//   res.status(201);
-//   next();
-// };
-
-// module.exports = signup;
-
 const bcrypt = require("bcryptjs");
 
 const { User } = require("../../models/user");
